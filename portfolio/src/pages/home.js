65e13979ac347efc45e7978adf5a658c89279abf
Chris Shelton <27889958+cjshelton@users.noreply.github.com>
2020-05-03 17:47:46 +0100

    Added text and an overlay to the home image.

--- portfolio/src/pages/home.js
+++ portfolio/src/pages/home.js
@@ -10,21 +10,62 @@ const seo = {
         ASP.NET Core and Node.js`,
 }
 
+const bold = css`
+    font-weight: 400;
+`
+
 const imageContainer = css`
     width: 100%;
-    height: 400px;
+    min-height: 250px;
     background-color: #d5d5d5; /* Base colour from the image to show before it loads. */
     background-image: url("./images/home.jpg");
     background-repeat: no-repeat;
     background-size: cover;
     background-position: bottom center;
+    position: relative;
+
+    .overlay {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background-color: rgba(18, 26, 33, 0.3);
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        padding: 0 40px;
+        text-align: center;
+
+        h1 {
+            font-size: 40px;
+            font-weight: 400;
+        }
+
+        h2 {
+            font-size: 20px;
+            font-weight: 300;
+        }
+    }
 `
 
 const HomePage = () => {
     return (
         <Layout page="Home">
             <SEO title={seo.title} description={seo.description} />
-            <div css={imageContainer}></div>
+            <div css={imageContainer}>
+                <div class="overlay">
+                    <h1>Portfolio</h1>
+                    <h2>
+                        Specialising in full-stack web application development
+                        using <span css={bold}>ASP.NET Core</span> and{" "}
+                        <span css={bold}>Node.js</span>
+                    </h2>
+                </div>
+            </div>
             <h1 style={{ color: "black" }}>Home</h1>
         </Layout>
     )
