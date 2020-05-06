cfb9079755a967857744eda5ad3c4bfe435056cd
Chris Shelton <27889958+cjshelton@users.noreply.github.com>
2020-05-06 22:06:14 +0100

    Added first round of styling to the 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,84 @@
 import React from "react"
+import { Global } from "@emotion/core"
+import { css } from "@emotion/core"
+import styled from "@emotion/styled"
 
 import SEO from "../components/seo"
+import Footer from "../components/footer"
+import Button from "../components/btn"
+
+import globalStyles from "../styles/global"
+import { colours } from "../styles/variables"
+
+// Ensure icon CSS is loaded immediately to prevent large icon sizes on page load.
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFrown } from "@fortawesome/free-regular-svg-icons"
+
+const seo = {
+    title: "Page Not Found",
+    description: `Portfolio for Chris Shelton. Leeds-based software engineer,
+        specialising in full-stack web application development using
+        ASP.NET Core and Node.js`,
+}
+
+const container = css`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    background-color: ${colours.primary};
+`
+
+const Content = styled.div`
+    flex-grow: 1;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    .sad-face {
+        font-family: "Roboto", sans-serif;
+        font-weight: 900;
+        font-size: 140px;
+        color: ${colours.primaryMediumDark};
+    }
+
+    .title {
+        font-size: 35px;
+        margin-top: 20px;
+    }
+
+    .message {
+        margin-top: 5px;
+        font-size: 18px;
+        margin-bottom: 40px;
+    }
+`
+
+const FooterContainer = styled.div`
+    flex-shrink: 1;
+    padding: 10px;
+`
 
 const NotFoundPage = () => (
-    <div>
-        <SEO title="404: Not found" />
-        <h1>NOT FOUND</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div css={container}>
+        <SEO title={seo.title} description={seo.description} />
+        <Global styles={globalStyles} />
+        <Content>
+            <div className="sad-face">:(</div>
+            <h1 className="title">Page Not Found</h1>
+            <p className="message">
+                Oops! The page you requested does not exist.
+            </p>
+            <Button medium to="/home">
+                Home
+            </Button>
+        </Content>
+
+        <FooterContainer>
+            <Footer></Footer>
+        </FooterContainer>
     </div>
 )
 
