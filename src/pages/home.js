62a92918a00e10071947db2b1e061f9dbb8bd001
Chris Shelton <27889958+cjshelton@users.noreply.github.com>
2020-05-08 09:40:14 +0100

    Moved layout files to a new directory.

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,7 @@ import React from "react"
 import { css } from "@emotion/core"
 import styled from "@emotion/styled"
 import SEO from "../components/seo"
-import Layout from "../components/layout"
+import Layout from "../components/layouts/site"
 
 import { sizes, colours } from "../styles/variables"
 
