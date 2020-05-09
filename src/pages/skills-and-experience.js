5fb331f25342e7b0cf6966c6fabfd13f8f3143b3
Chris Shelton <27889958+cjshelton@users.noreply.github.com>
2020-05-09 12:41:33 +0100

    Added professional experience.

--- src/pages/skills-and-experience.js
+++ src/pages/skills-and-experience.js
@@ -1,5 +1,11 @@
 import React from "react"
+import { css } from "@emotion/core"
+import styled from "@emotion/styled"
+
 import Page from "../components/layouts/page"
+import UolLogo from "../components/uol-logo"
+
+import experienceData from "../data/experience.json"
 
 const seo = {
     title: "Skills and Experience",
@@ -7,6 +13,25 @@ const seo = {
         specialising in full-stack web application development using
         ASP.NET Core and Node.js`,
 }
+
+const uolLogoContainer = css`
+    width: 200px;
+    margin: 20px auto;
+`
+
+const ExperienceEntry = styled.div`
+    .experience-company {
+        font-size: 20px;
+        font-weight: 600;
+        margin: 0 10px;
+    }
+
+    .experience-title {
+        font-size: 16px;
+        margin-bottom: 10px;
+        margin: 0 10px 10px 10px;
+    }
+`
 
 const headerJsx = (
     <div>
@@ -21,18 +46,14 @@ const contentJsx = (
     <div>
         <section>
             <h1 className="section-header">Education</h1>
+            <div css={uolLogoContainer}>
+                <UolLogo></UolLogo>
+            </div>
             <p className="section">
                 I proudly graduated from The University of Leeds in 2016, with a
                 First-class (Hons) in Computer Science. I also received an award
                 for academic excellence two years in a row during my time at
                 University.
-            </p>
-            <p className="section">
-                Prior to going to university, I had very little experience of
-                computing. In all honesty, it wasn&apos;t something I was even
-                thinking about as a career; I wasn&apos;t sure what I wanted to
-                do. But getting started in programming, I was instantly engaged,
-                and have been since.
             </p>
             <p className="section">
                 I thoroughly enjoyed my degree. I enjoyed the challenges it
@@ -44,40 +65,23 @@ const contentJsx = (
         </section>
         <section>
             <h1 className="section-header">Professional Experience</h1>
-            <p className="section">
-                Since graduating from university in 2016, I have worked as a
-                Technical Specialist at Bluesmith Information Systems in Leeds,
-                focusing on full-stack web development for enterprise
-                applications.
-            </p>
-            <p className="section">
-                I have 3 years' experience in the Microsoft tech-stack,
-                designing and building ASP.NET and SQL Server applications, with
-                the last year working with ASP.NET Core.
-            </p>
-            <p className="section">
-                More recently, I have been up-skilling in JavaScript, working
-                exclusively with Node.js and front-end technologies like
-                Knockout and Vue.
-            </p>
+
+            {experienceData.map(({ company, title, period, description }) => {
+                return (
+                    <ExperienceEntry>
+                        <h2 className="experience-company">
+                            {company} - {period}
+                        </h2>
+                        <h3 className="experience-title">{title}</h3>
+                        <p className="section">{description}</p>
+                    </ExperienceEntry>
+                )
+            })}
         </section>
         <section>
             <h1 className="section-header">Core Skills</h1>
             <p className="section">
-                Since graduating from university in 2016, I have worked as a
-                Technical Specialist at Bluesmith Information Systems in Leeds,
-                focusing on full-stack web development for enterprise
-                applications.
-            </p>
-            <p className="section">
-                I have 3 years' experience in the Microsoft tech-stack,
-                designing and building ASP.NET and SQL Server applications, with
-                the last year working with ASP.NET Core.
-            </p>
-            <p className="section">
-                More recently, I have been up-skilling in JavaScript, working
-                exclusively with Node.js and front-end technologies like
-                Knockout and Vue.
+                My core skills gained through professional experience.
             </p>
         </section>
         <section>
