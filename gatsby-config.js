5e73d4e86d865097466d9144c628fa4453d53506
Chris Shelton <27889958+cjshelton@users.noreply.github.com>
2021-09-27 00:04:31 +0100

    Install the Poppins Google font locally to avoid a network request

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,12 +70,6 @@ module.exports = {
                 icon: `src/images/mugshot-colour.jpg`, // This path is relative to the root of the site.
             },
         },
-        {
-            resolve: `gatsby-plugin-google-fonts`,
-            options: {
-                fonts: [`Poppins\:300,400`, `Roboto\:300,900`],
-            },
-        },
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,
