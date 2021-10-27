f4c290dc33280f3916a1c4b8491b0457773a6a95
Chris Shelton <27889958+cjshelton@users.noreply.github.com>
2021-10-27 22:40:46 +0100

    Reduce quality of tech logo images to reduce file sizes

--- src/components/tech-logo.js
+++ src/components/tech-logo.js
@@ -7,7 +7,7 @@ const Image = ({ image, altText }) => {
         query {
             cSharp: file(relativePath: { eq: "tech-logos/c-sharp.png" }) {
                 childImageSharp {
-                    fluid(maxHeight: 100, quality: 100) {
+                    fluid(maxHeight: 100, quality: 75) {
                         ...GatsbyImageSharpFluid
                     }
                 }
@@ -16,49 +16,56 @@ const Image = ({ image, altText }) => {
                 relativePath: { eq: "tech-logos/dot-net-core.png" }
             ) {
                 childImageSharp {
-                    fluid(maxWidth: 200, quality: 100) {
+                    fluid(maxWidth: 200, quality: 75) {
                         ...GatsbyImageSharpFluid
                     }
                 }
             }
             frontEnd: file(relativePath: { eq: "tech-logos/front-end.png" }) {
                 childImageSharp {
-                    fluid(maxWidth: 200, quality: 100) {
+                    fluid(maxWidth: 200, quality: 75) {
                         ...GatsbyImageSharpFluid
                     }
                 }
             }
             git: file(relativePath: { eq: "tech-logos/git.png" }) {
                 childImageSharp {
-                    fluid(maxWidth: 200, quality: 100) {
+                    fluid(maxWidth: 200, quality: 75) {
                         ...GatsbyImageSharpFluid
                     }
                 }
             }
             nodeJs: file(relativePath: { eq: "tech-logos/nodejs.png" }) {
                 childImageSharp {
-                    fluid(maxWidth: 200, quality: 100) {
+                    fluid(maxWidth: 200, quality: 75) {
                         ...GatsbyImageSharpFluid
                     }
                 }
             }
             sqlServer: file(relativePath: { eq: "tech-logos/sql-server.png" }) {
                 childImageSharp {
-                    fluid(maxWidth: 200, quality: 100) {
+                    fluid(maxWidth: 200, quality: 75) {
                         ...GatsbyImageSharpFluid
                     }
                 }
             }
             vue: file(relativePath: { eq: "tech-logos/vue.png" }) {
                 childImageSharp {
-                    fluid(maxWidth: 200, quality: 100) {
+                    fluid(maxWidth: 200, quality: 75) {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+            react: file(relativePath: { eq: "tech-logos/react.png" }) {
+                childImageSharp {
+                    fluid(maxWidth: 200, quality: 75) {
                         ...GatsbyImageSharpFluid
                     }
                 }
             }
             docker: file(relativePath: { eq: "tech-logos/docker.png" }) {
                 childImageSharp {
-                    fluid(maxWidth: 200, quality: 100) {
+                    fluid(maxWidth: 200, quality: 75) {
                         ...GatsbyImageSharpFluid
                     }
                 }
@@ -67,7 +74,7 @@ const Image = ({ image, altText }) => {
                 relativePath: { eq: "tech-logos/javascript.png" }
             ) {
                 childImageSharp {
-                    fluid(maxWidth: 200, quality: 100) {
+                    fluid(maxWidth: 200, quality: 75) {
                         ...GatsbyImageSharpFluid
                     }
                 }
@@ -76,28 +83,28 @@ const Image = ({ image, altText }) => {
                 relativePath: { eq: "tech-logos/typescript.png" }
             ) {
                 childImageSharp {
-                    fluid(maxWidth: 200, quality: 100) {
+                    fluid(maxWidth: 200, quality: 75) {
                         ...GatsbyImageSharpFluid
                     }
                 }
             }
             mongodb: file(relativePath: { eq: "tech-logos/mongo-db.png" }) {
                 childImageSharp {
-                    fluid(maxWidth: 200, quality: 100) {
+                    fluid(maxWidth: 200, quality: 75) {
                         ...GatsbyImageSharpFluid
                     }
                 }
             }
             openApi: file(relativePath: { eq: "tech-logos/open-api.png" }) {
                 childImageSharp {
-                    fluid(maxWidth: 200, quality: 100) {
+                    fluid(maxWidth: 200, quality: 75) {
                         ...GatsbyImageSharpFluid
                     }
                 }
             }
             jest: file(relativePath: { eq: "tech-logos/jest.png" }) {
                 childImageSharp {
-                    fluid(maxWidth: 200, quality: 100) {
+                    fluid(maxWidth: 200, quality: 75) {
                         ...GatsbyImageSharpFluid
                     }
                 }
