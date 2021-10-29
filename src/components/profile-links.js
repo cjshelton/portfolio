63fa40d78b700e84c359d255ec2c9224bce9dddc
Chris Shelton <27889958+cjshelton@users.noreply.github.com>
2021-10-30 00:43:36 +0100

    Migrate the profile links to use Static Image

--- src/components/profile-links.js
+++ src/components/profile-links.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
-// import Img from "gatsby-image";
+import { StaticImage } from "gatsby-plugin-image";
 import { colours } from "../styles/variables";
 import { css } from "@emotion/react";
 
@@ -20,34 +20,9 @@ const profileLinkIconStyles = css`
     color: ${colours.primaryTextLight};
 `;
 
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `useStaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 const ProfileLinks = () => {
     const data = useStaticQuery(graphql`
         query {
-            gitHubImage: file(relativePath: { eq: "github.png" }) {
-                childImageSharp {
-                    fluid(maxWidth: 50, quality: 100) {
-                        ...GatsbyImageSharpFluid
-                    }
-                }
-            }
-            linkedInImage: file(relativePath: { eq: "linked-in.png" }) {
-                childImageSharp {
-                    fluid(maxWidth: 50, quality: 100) {
-                        ...GatsbyImageSharpFluid
-                    }
-                }
-            }
             site {
                 siteMetadata {
                     profileLinks {
@@ -65,25 +40,31 @@ const ProfileLinks = () => {
                 href={data.site.siteMetadata.profileLinks.github}
                 aria-label="Github profile"
             >
-                {/* <Img
-                    fluid={data.gitHubImage.childImageSharp.fluid}
+                <StaticImage
+                    src="../images/github.png"
                     alt="GitHub logo"
+                    width={30}
+                    layout="fixed"
+                    placeholder="tracedSVG"
+                    quality={80}
                     css={profileLinkIconStyles}
-                /> */}
-                <h1>TEST 1</h1>
+                />
             </a>
 
             <a
                 href={data.site.siteMetadata.profileLinks.linkedIn}
                 aria-label="LinkedIn profile"
             >
-                {/* <Img
-                    fluid={data.linkedInImage.childImageSharp.fluid}
+                <StaticImage
+                    src="../images/linked-in.png"
                     alt="LinkedIn logo"
+                    width={30}
+                    layout="fixed"
+                    placeholder="tracedSVG"
+                    quality={80}
                     css={profileLinkIconStyles}
                     style={{ marginLeft: "10px" }}
-                /> */}
-                <h1>TEST 2</h1>
+                />
             </a>
             <a href="mailto:cjshelton@outlook.com" aria-label="Email me">
                 <div css={profileLinkIconStyles} style={{ marginLeft: "10px" }}>
