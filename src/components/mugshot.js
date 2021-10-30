cf96822d5290b3c7ac3e363b46824071976a418d
Chris Shelton <27889958+cjshelton@users.noreply.github.com>
2021-10-30 21:54:02 +0100

    Fix an issue with the mugshot rendering differently across browsers

--- src/components/mugshot.js
+++ src/components/mugshot.js
@@ -2,6 +2,11 @@ import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
 import { colours } from "../styles/variables";
 
+const imageStyles = {
+    borderRadius: "50%",
+    boxShadow: `0px 6px 6px ${colours.primaryDark}`,
+};
+
 const Image = () => {
     return (
         <StaticImage
@@ -9,13 +14,12 @@ const Image = () => {
             alt="Chris Shelton mugshot"
             width={200}
             layout="constrained"
-            placeholder="blurred"
+            placeholder="none"
             quality={100}
-            style={{
-                WebkitBorderRadius: `50%`,
-                borderRadius: `50%`,
-                boxShadow: `0px 6px 6px ${colours.primaryDark}`,
-            }}
+            // Image styles need to be provided to both props to account for inconsistent
+            // behaviour on different browsers.
+            style={imageStyles}
+            imgStyle={imageStyles}
         />
     );
 };
