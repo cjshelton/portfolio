a78f2c4f3b28e8277597bf6545ee876276fb9386
Chris Shelton <27889958+cjshelton@users.noreply.github.com>
2021-11-21 20:42:24 +0000

    Improve the styling of the navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -11,6 +11,7 @@ import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import Mugshot from "../components/mugshot";
 import Nav from "../components/nav";
 
+import { BackgroundGradientStyles } from "../styles/shared";
 import { colours, sizes } from "../styles/variables";
 
 const TopNav = styled.div`
@@ -20,6 +21,7 @@ const TopNav = styled.div`
     min-height: 60px;
     background-color: ${colours.primary};
     padding: 5px 10px 7px 10px;
+    ${BackgroundGradientStyles}
 
     flex-shrink: 1;
 
@@ -83,23 +85,23 @@ const navContainer = css`
                 font-size: 18px;
 
                 li {
-                    background-color: ${colours.primaryMediumDark};
-                    padding: 8px 0;
-                    margin-bottom: 10px;
                     display: block;
                     width: 100%;
+                    padding: 8px 0;
+                    margin-bottom: 10px;
+
+                    background-color: ${colours.primaryMediumLight};
                     text-align: center;
+                    border: 1px solid transparent;
 
                     &:not(:first-of-type) {
                         margin-left: 0;
                     }
 
+                    &.current,
                     &:hover {
                         background-color: ${colours.primaryDark};
-                    }
-
-                    &.current {
-                        background-color: ${colours.primaryDark};
+                        border-color: ${colours.white};
                     }
                 }
             }
@@ -111,15 +113,15 @@ const navButtonContainer = css`
     display: none;
 
     button {
-        width: 30px;
-        font-size: 20px;
-        color: white;
-        background-color: transparent;
         padding: 2px 5px;
-        border-radius: 5px;
+
+        font-size: 30px;
+        color: ${colours.white};
+        background-color: transparent;
+        border: none;
 
         &:hover {
-            background-color: ${colours.primaryDark};
+            cursor: pointer;
         }
     }
 
